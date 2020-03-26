d77e6ce46106858e0764c2cda95fc35c4fbf5c59
Micah McCallum <micahmccallum@outlook.com>
2020-03-26 01:54:37 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,13 +1,13 @@
 def oxford_comma(array)
- if array.size == 1
+ if array.length == 1
    array.join
- elsif array.size == 2
+ elsif array.length == 2
    array.join(" and ")
  else
    string = ""
    current = 0
    while current < array.length - 2
-     string << array[0] + ", "
+     string << array[current] + ", "
      current += 1
    end
    string << array[current - 1] + " and " + array[current]
